617fb7120d39b8834df67addc7cf6ab019c2507b
Dmitriy <fed3375546@yandex.ru>
2020-07-22 19:20:43 +0300

    completed the task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanNext =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanNext.getMessage());
+
+        Cat murka = (Cat) applicationContext.getBean(("cat"));
+        System.out.println(murka.getMessage());
+
+        Cat barsik = (Cat) applicationContext.getBean(("cat"));
+        System.out.println(barsik.getMessage());
+
+        System.out.println("(bean == beanNext) = " + (bean == beanNext));
+        System.out.println("(murka == barsik) = " + (murka == barsik));
     }
 }
